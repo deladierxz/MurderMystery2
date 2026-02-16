431bbd15a099bf34bda82578bf6dc7dda81d4a88
OverDrive <breno.y2k@gmail.com>
2026-02-16 20:25:19 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjkwNGU5NTg3LTQ0ZDAtNGQ1YS04MmFiLTJhYzlmZWFjZjVhZSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6ImE1ZjEyYjhhLTgyZWItNDE4OC1hNTRlLTdlOGEwODA1YjJiMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
